311242f020f6e6886253d88700554ad5d458109a
Muhammad Ivan Aldorino <ivanaldorino@gmail.com>
2025-12-13 17:56:54 +0700

    Spencal Reimagined Release

--- login.php
+++ login.php
@@ -1,12 +1,15 @@
 <?php
-// Tentukan URL Login Valselt (Port 9000)
-$valselt_login_url = "https://valseltid.ivanaldorino.web.id/login.php";
+// Tentukan URL Login Valselt
+$valselt_login_url = "https://valseltid.ivanaldorino.web.id/login";
 
-// Tentukan URL Callback Spencal (Port 1649)
-// Ini adalah alamat di mana Valselt harus mengembalikan user setelah login sukses
+// Tentukan URL Callback Spencal
 $my_callback_url = "https://spencal.ivanaldorino.web.id/auth_callback.php";
 
+// --- PERUBAHAN DISINI ---
+// Kita ubah URL menjadi kode acak (Base64) agar tidak diblokir firewall server
+$encoded_url = base64_encode($my_callback_url);
+
 // Redirect User ke Valselt
-header("Location: " . $valselt_login_url . "?redirect_to=" . urlencode($my_callback_url));
+header("Location: " . $valselt_login_url . "?redirect_to=" . urlencode($encoded_url));
 exit();
 ?>
